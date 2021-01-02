bbed5518631007442398f8945789c4cc39567dce
徐永进 <hzxuyongjin@corp.netease.com>
2021-01-02 19:22:27 +0800

    add: maxRec

---
+++ algorithm/src/main/java/com/xyjawq/learn/algorithm/stack/MaxRec.java
@@ -0,0 +1,15 @@
+package com.xyjawq.learn.algorithm.stack;
+
+/**
+* @author: xyjawq
+* @date: 2021/1/2
+ * 8. 给定一个整型矩阵map, 其中的值只有0和1,求其中全是1的所有矩形区域中，最大的矩形区域为1的数量。
+ * 例如：
+ * 1 0 1 1
+ * 1 1 1 1
+ * 1 1 1 0
+ * 其中最大的矩形区域有6个1，所以返回6。
+**/
+public class MaxRec {
+
+}
